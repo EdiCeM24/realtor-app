3d271529d58a0796054fb4dc7f0e522ff2a0a92b
Marcus Edidiong Cletus <edi_mars@outlook.com>
2024-06-25 17:31:11 +0100

    Install all the modified changes made.

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -9,6 +9,10 @@ export default function SignIn() {
   const { email, password } = formData;
   function onChange(e) {
     //console.log(e.target.value);
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }))
   }
   return (
     <section>
@@ -19,10 +23,11 @@ export default function SignIn() {
            <img src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?q=80&w=1373&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Key" className="w-full rounded-2xl" />
         </div>
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-          <form>
-             <input className="w-full" type="email" id="email" value={email} onChange={onChange}/>
+          <form className='p-8'>
+             <input type="email" id="email" value={email} onChange={onChange} placeholder='Enter Your email address' className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"/>
 
-             <input type="password" className="w-full" id="" value={password}/>
+             <input type="password" id="password" value={password} onChange={onChange} placeholder='Enter your password' 
+              className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out" />
           </form>
         </div>
       </div>
